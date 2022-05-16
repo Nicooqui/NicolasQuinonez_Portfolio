eb92af145691584aa15aa0d95ef44d77778ca5d1
Nico Quinonez <nicolasquinionez@gmail.com>
2022-05-16 14:52:40 -0300

    navbar developed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import './Home.css';
 
 export const Home = () => {
 
     return (
         <div className='container flex flex-col items-center'>
-            <div className='testUno my-12'>
+            <div className='testUno mt-24'>
                 <img src={`${process.env.PUBLIC_URL}/images/nico_ph.jpg`} className='h-full rounded'/>
             </div>
             <div>
                 <div className='flex justify-center'>
-                    <img src={`${process.env.PUBLIC_URL}/images/hand_wave.png`} className='w-12 h-12'/>
-                    <h2 className='ml-4 text-4xl font-bold place-self-center'>Hi there, I'm</h2>
+                    <img src={`${process.env.PUBLIC_URL}/images/hand_wave.png`} className='w-12 h-12 mt-4'/>
+                    <h2 className='ml-4 text-4xl py-6 font-bold place-self-center'>Hi there, I'm</h2>
                 </div>
-                <div className='flex justify-center'>
-                    <h1 className='text-5xl py-14 font-bold'>Nicolás Quiñonez</h1>
+                <div className='flex flex-col justify-center text-center'>
+                    <h1 className='text-6xl py-12 font-bold w-62 text-[#6D21B9]'>Nicolás Quiñonez</h1>
+                    <h2 className='text-5xl py-12 font-semibold'>Frontend Developer</h2>
                 </div>
                 <div></div>
             </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import { GoGrabber } from 'react-icons/go';
-
+import React, { useState } from 'react';
+import './Navbar.css';
 
 export const Navbar = () => {
+  let Links = [
+    { name: 'Home', link: '/' },
+    { name: 'About', link: '/' },
+    { name: 'Portfolio', link: '/' },
+    { name: 'Contact', link: '/' },
+  ];
+
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-        <nav className='flex items-center bg-[#6D21B9] h-14'>
-            <div className='flex-1'>
-                <h1 className='text-white p-2 text-lg font-medium'>Nicolás Quiñonez</h1>
-            </div>
+      <nav className='w-full fixed top-0 left-0'>
+        <div className='md:flex items-center justify-between bg-[#6D21B9] py-4 md:px-10 px-7'>
+          <div className='font-bold text-3xl cursor-pointer flex items-center'>
+            <h1>Nicolás Quiñonez</h1>
+          </div>
 
-            <div className='flex-1 flex justify-end'>
-                <GoGrabber className='w-14 h-14 text-white'/>
-            </div>
-        </nav>
+          <div onClick={() => setOpen(!open)} className='text-4xl absolute right-8 top-4 cursor-pointer md:hidden'>
+            <ion-icon name={open ? 'close':'menu'}></ion-icon>
+          </div>
+          <ul className={`bg-[#6D21B9] md:flex md:items-center absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 opacity-100':'top-[-490px]'} md:opacity-100 `}>
+            {Links.map((Link) => (
+              <li key={Link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+                <a href={Link.link} className='link-underline link-underline-black'>{Link.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
